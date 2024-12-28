74d8bdaf45a3797ac52aebd81c6d071c69e7eff6
HATEMNadia <hatemsophie7@gmail.com>
2024-12-28 02:58:51 +0200

    Create nano deploy.sh
    Signed-off-by: HATEMNadia <hatemsophie7@gmail.com>

---
+++ nano deploy.sh
@@ -0,0 +1,5 @@
+git init  # لإنشاء مستودع Git محلي
+git add .  # لإضافة جميع الملفات للمستودع
+git commit -m "الرسالة التي تصف التغييرات"
+git remote add origin https://github.com/اسمك/اسم-المستودع.git  # لربط المستودع المحلي مع مستودع GitHub
+git push -u origin main  # لرفع الملفات إلى GitH
