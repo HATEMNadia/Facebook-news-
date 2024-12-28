ed4d2183b0e9d64eda985712383ad40da6a7b671
HATEMNadia <hatemsophie7@gmail.com>
2024-12-28 02:40:35 +0200

    Create chmod +x deploy.sh
    Signed-off-by: HATEMNadia <hatemsophie7@gmail.com>

---
+++ chmod +x deploy.sh
@@ -0,0 +1,7 @@
+
+git init  # لإنشاء مستودع Git محلي
+git add .  # لإضافة جميع الملفات للمستودع
+git commit -m "الرسالة التي تصف التغييرات"
+git remote add origin https://github.com/اسمك/اسم-المستودع.git  # لربط المستودع المحلي مع مستودع GitHub
+git push -u origin master  # لرفع الملفات إلى GitHub
+
